cbb5f7af6757b297ebf12599ac2d16526e93f862
sylviavitoria <sylvia.souza@dbserver.com.br>
2025-05-09 11:23:02 -0300

    refactor: corrigindo instâncias de Bruxo e removendo casts desnecessários

--- src/main/java/com/sylviavitoria/Main.java
+++ src/main/java/com/sylviavitoria/Main.java
@@ -2,14 +2,15 @@ package com.sylviavitoria;
 
 public class Main {
     public static void main(String[] args) {
-        Bruxo harry = new BruxoGrifinoria("Harry Potter");
+        BruxoGrifinoria harry = new BruxoGrifinoria("Harry Potter");
+        BruxoSonserina draco = new BruxoSonserina("Draco Malfoy");
+
         harry.mostrarInformacoes();
-        ((Magia) harry).lancarFeitico(); 
+        harry.lancarFeitico();
 
         System.out.println();
 
-        Bruxo draco = new BruxoSonserina("Draco Malfoy");
         draco.mostrarInformacoes();
-        ((Magia) draco).lancarFeitico(); 
+        draco.lancarFeitico();
     }
 }
